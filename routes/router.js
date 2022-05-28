938bdb67ca18fd28e8b8de9e72f4a1c2b78b68ba
Daniel Liberman <liberman.daniel@outlook.com>
2022-05-28 12:30:13 +0300

    Delete button

--- routes/router.js
+++ routes/router.js
@@ -4,23 +4,23 @@ const dbs = require("../config/sqlite")
 const passport = require('passport');
 const LocalStrategy = require("passport-local").Strategy
 const User = require("../models/User")
-const {ensureAuthenticated} = require('../config/auth');
+const { ensureAuthenticated } = require('../config/auth');
 
 const router = express.Router();
 
-router.get("/", (req,res) => {
+router.get("/", (req, res) => {
     res.render("homepage")
 })
 
-router.get("/dashboard",ensureAuthenticated, async (req,res) => {
+router.get("/dashboard", ensureAuthenticated, async (req, res) => {
     let name = req.query.filename
     let query = 'select * from employees where Firstname=\"' + name + ';'
     let params = []
     let msg
 
     const username = req.user.username
-    const usernameDB = await User.find({username: username}).exec()
-    
+    const usernameDB = await User.find({ username: username }).exec()
+
     if (usernameDB[0].rights) {
         console.log("admin")
         msg = "Congratulations you've gained admin access!"
@@ -31,48 +31,47 @@ router.get("/dashboard",ensureAuthenticated, async (req,res) => {
 
     dbs.all(query, params, (err, rows) => {
         if (err) {
-            res.render("dashboard", {"message":msg})
+            res.render("dashboard", { "message": msg })
             return;
         }
-        if(rows) {
-            res.render("dashboard", {"params":rows, "message":msg})
+        if (rows) {
+            res.render("dashboard", { "params": rows, "message": msg })
         }
     });
 })
 
-router.get("/register", (req,res) => {
-    res.render("register", {"error": ""})
+router.get("/register", (req, res) => {
+    res.render("register", { "error": "" })
 })
 
 
-router.post("/register", async (req,res) => {
+router.post("/register", async (req, res) => {
     // toggle check box - true/false 
-    const adminRights =  req.body.admin ? true : false
+    const adminRights = req.body.admin ? true : false
     const newUsername = req.body.username
     const newPassword = req.body.password
 
-    const sameUsername = await User.find({username: newUsername}).exec()
+    const sameUsername = await User.find({ username: newUsername }).exec()
     if (sameUsername.length) {
-        res.render("register", {"error": `${newUsername} is taken try another one`})
+        res.render("register", { "error": `${newUsername} is taken try another one` })
     }
     else {
         const doc = new User({
-            username: newUsername, 
+            username: newUsername,
             password: newPassword,
             rights: adminRights
         })
-        console.log(doc)
         doc.save()
             .then((response) => console.log(response))
-        res.render("register", {"error":"You have registered successfully"})
+        res.render("register", { "error": "You have registered successfully" })
     }
 })
 
-router.get("/login", (req,res) => {
-    res.render("login", {"error": ""})
+router.get("/login", (req, res) => {
+    res.render("login", { "error": "" })
 })
 
-router.post("/login", async (req,res,next) => {
+router.post("/login", async (req, res, next) => {
     passport.authenticate('local', {
         successRedirect: '/dashboard',
         failureRedirect: '/register',
@@ -82,9 +81,24 @@ router.post("/login", async (req,res,next) => {
 router.get('/logout', async (req, res) => {
     req.logout();
     res.redirect('/');
-  });
+});
 
-router.get(("*"), (req,res) => {
+router.post('/delete', async (req, res) => {
+    mongoose.connection.db.dropCollection("users", (err,result) => {
+        console.log("Collection droped");
+    })
+    const doc = new User({
+        username: 'admin',
+        password: 'spongebob',
+        rights: true
+    })
+    doc.save()
+        .then((response) => console.log(response))
+    res.status(200).redirect('/')
+
+})
+
+router.get(("*"), (req, res) => {
     res.redirect('/')
 })
 
